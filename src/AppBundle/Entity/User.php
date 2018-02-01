3a5d887b810bc8437b50040df94ac2a2040ceb2f
Jeanne <jeanne.tiramani@hotmail.fr>
2018-02-01 14:34:05 +0100

    ajout des champs supplémentaires

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -15,7 +15,7 @@ class User extends BaseUser
 // /**
 //  * User
 //  *
-//  * @ORM\Table(name="user")
+//  * @ORM\Table(name="usr_user")
 //  * @ORM\Entity(repositoryClass="AppBundle\Repository\UserRepository")
 //  */
 // class User
@@ -23,7 +23,7 @@ class User extends BaseUser
     /**
      * @var int
      *
-     * @ORM\Column(name="use_oid", type="integer")
+     * @ORM\Column(name="usr_oid", type="integer")
      * @ORM\Id
      * @ORM\GeneratedValue(strategy="AUTO")
      */
@@ -39,5 +39,100 @@ class User extends BaseUser
     {
         return $this->id;
     }
-}
 
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="usr_firstname", type="string", length=255, nullable=true)
+     */
+    private $firstname;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="usr_lastname", type="string", length=255, nullable=true)
+     */
+    private $lastname;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="usr_phone_number", type="string", length=255, nullable=true)
+     */
+    private $phoneNumber;
+
+
+
+    /**
+     * Set firstname
+     *
+     * @param string $firstname
+     *
+     * @return User
+     */
+    public function setFirstname($firstname)
+    {
+        $this->firstname = $firstname;
+
+        return $this;
+    }
+
+    /**
+     * Get firstname
+     *
+     * @return string
+     */
+    public function getFirstname()
+    {
+        return $this->firstname;
+    }
+
+    /**
+     * Set lastname
+     *
+     * @param string $lastname
+     *
+     * @return User
+     */
+    public function setLastname($lastname)
+    {
+        $this->lastname = $lastname;
+
+        return $this;
+    }
+
+    /**
+     * Get lastname
+     *
+     * @return string
+     */
+    public function getLastname()
+    {
+        return $this->lastname;
+    }
+
+    /**
+     * Set phoneNumber
+     *
+     * @param string $phoneNumber
+     *
+     * @return User
+     */
+    public function setPhoneNumber($phoneNumber)
+    {
+        $this->phoneNumber = $phoneNumber;
+
+        return $this;
+    }
+
+    /**
+     * Get phoneNumber
+     *
+     * @return string
+     */
+    public function getPhoneNumber()
+    {
+        return $this->phoneNumber;
+    }
+}
